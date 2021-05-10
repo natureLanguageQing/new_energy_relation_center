de80117c678bf0fa33de77454aa00ec271fb78e7
yanguangqing <yanguangqing@gennlife.com>
2021-05-10 17:35:10 +0800

    加注释 二级关联企业pid导出

--- KG_match_text/new_energy.py
+++ KG_match_text/new_energy.py
@@ -1,38 +1,39 @@
 # 探索公司二级关系中的公司pid
 import json
 import os
+import pandas as pd
 
-# 知识图谱地址
 from json import JSONDecodeError
 
+# 知识图谱地址
 knowledge_path = "../company_knowledge_base/company_detail_company"
 # 新闻地址
 news_path = "../chinese_news_double"
+# 二级相关企业pid保存地址
+second_pid_path = "second_pid.csv"
 
 news_path_list = os.listdir(news_path)
 knowledge_path_list = os.listdir(knowledge_path)
 print(len(knowledge_path_list))
-second_pid = open("second_pid.csv", "w", encoding="utf-8")
+second_pid = open(second_pid_path, "w", encoding="utf-8")
 for knowledge_path_one in knowledge_path_list:
     try:
         knowledge_json = json.load(open(os.path.join(knowledge_path, knowledge_path_one), "r", encoding="utf8"))
     except JSONDecodeError:
+        # 因为爬虫的时候出现的问题导致企业知识图谱中可能存在一些问题
         print("JSONDecodeError", os.path.join(knowledge_path, knowledge_path_one))
         continue
     for total_list in knowledge_json['data'].values():
-        print(total_list)
         if "pid" in total_list:
             print(total_list["pid"])
             second_pid.writelines(total_list["pid"] + "\n")
         if "list" in total_list:
             for second_total in total_list["list"]:
                 if "pid" in second_total:
-                    print(second_total["pid"])
                     try:
                         if int(second_total["pid"]):
                             second_pid.writelines(second_total["pid"] + "\n")
                     except ValueError:
                         continue
-import pandas as pd
 
-pd.read_csv("second_pid.csv").drop_duplicates().to_csv("second_pid.csv", index=False)
+pd.read_csv(second_pid_path).drop_duplicates().to_csv(second_pid_path, index=False)
